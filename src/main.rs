e40cfaf71888fec9a794327c806dd2e8397c09ac
Jonathan Ringer <jonringer117@gmail.com>
2020-07-18 09:03:42 -0700

    Implement completion generation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod cli;
 mod types;
 
+use std::io;
+use clap::Shell;
+
 fn main() {
     let m = cli::build_cli().get_matches();
+    println!("{:?}", m);
+
+    match m.subcommand() {
+        ("completions", Some(m)) => { cli::build_cli().gen_completions_to("nix-template", m.value_of("SHELL").unwrap().parse::<Shell>().unwrap(), &mut io::stdout())},
+        _ => {},
+    }
 }
