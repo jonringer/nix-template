1f2db0360145974d48ac3708702941f5457da3e2
Jonathan Ringer <jonringer117@gmail.com>
2020-08-30 16:05:36 -0700

    Refactor filepath logic

--- src/file_path.rs
+++ src/file_path.rs
@@ -20,38 +20,37 @@ pub fn nix_file_paths(
             exit(1);
         }
 
+        let mut radix: PathBuf;
         if matches.occurrences_of("PATH") == 0 {
             // default to nixpkgs path
             if *template == Template::python {
-                let mut radix = PathBuf::from("development/python-modules/");
-                radix.push(&pname);
-                let mut file_path = PathBuf::from(&nixpkgs_root);
-                file_path.push("pkgs");
-                file_path.push(&radix);
-                file_path.push("default.nix");
-                let mut nix_path = PathBuf::from("..");
-                nix_path.push(&radix);
-                return (file_path, nix_path);
+                eprintln!("No [PATH] provided, defaulting to \"pkgs/development/python-modules/\"");
+                radix = PathBuf::from("development/python-modules/");
             } else {
-                eprintln!("Template '{}' does not have a known path default for the nixpkgs repo, please provide a PATH (E.g. nix-template stdenv --pname mypackage --nixpkgs pkgs/applications/misc/", template);
-                exit(1);
+                eprintln!("No [PATH] provided, defaulting to \"pkgs/applications/misc/\"");
+                radix = PathBuf::from("applications/misc");
             }
         } else {
-            let radix = path.strip_prefix("pkgs").unwrap_or(path);
-
-            let mut file_path = PathBuf::from(&nixpkgs_root);
-            file_path.push("pkgs");
-            file_path.push(&radix);
-            // may have specified a specific nix file (E.g path/package.nix)
-            if file_path.extension() != Some(std::ffi::OsStr::new("nix")) {
-                file_path.push("default.nix");
-            }
-
-            let mut nix_path = PathBuf::from("..");
-            nix_path.push(&radix);
-
-            return (file_path, nix_path);
+            radix = path.strip_prefix("pkgs").unwrap_or(path).to_path_buf();
         }
+
+        if !radix.ends_with(&pname) && radix.extension() != Some(std::ffi::OsStr::new("nix")) {
+            radix.push(&pname);
+        }
+
+        let mut nix_path = PathBuf::from("..");
+        nix_path.push(&radix);
+
+        let mut file_path = PathBuf::from(&nixpkgs_root);
+        file_path.push("pkgs");
+        file_path.push(&radix);
+
+        // may have specified a specific nix file (E.g path/package.nix)
+        if file_path.is_dir() || file_path.extension() != Some(std::ffi::OsStr::new("nix")) {
+            file_path = file_path.join("default.nix");
+        }
+
+        return (file_path, nix_path);
     }
 
     (path.to_path_buf(), PathBuf::from(""))
@@ -63,25 +62,49 @@ mod tests {
     use crate::cli::{build_cli, validate_and_serialize_matches};
     use pretty_assertions::assert_eq;
     use serial_test::serial;
+    use clap::ArgMatches;
+
+    fn assert_paths(m: ArgMatches, expected: (PathBuf, PathBuf)) {
+        let info = validate_and_serialize_matches(&m, None);
+        let actual = (info.path_to_write, info.top_level_path);
+
+        assert_eq!(expected, actual);
+    }
 
     #[test]
     #[serial]
     fn test_python() {
         let m =
             build_cli().get_matches_from(vec!["nix-template", "python", "-n", "-p", "requests"]);
-        let info = validate_and_serialize_matches(&m, None);
-        let nixpkgs_root: String = "".to_owned();
         let expected = (
             PathBuf::from("pkgs/development/python-modules/requests/default.nix"),
             PathBuf::from("../development/python-modules/requests"),
         );
-        let actual = nix_file_paths(
-            &m,
-            &info.template,
-            &info.path_to_write,
-            &info.pname,
-            &nixpkgs_root,
-        );
-        assert_eq!(expected, actual);
+        assert_paths(m, expected);
     }
+
+    #[test]
+    #[serial]
+    fn test_stdenv_no_path() {
+        let m =
+            build_cli().get_matches_from(vec!["nix-template", "stdenv", "-n", "-p", "mypackage"]);
+        let expected = (
+            PathBuf::from("pkgs/applications/misc/mypackage/default.nix"),
+            PathBuf::from("../applications/misc/mypackage"),
+        );
+        assert_paths(m, expected);
+    }
+
+    #[test]
+    #[serial]
+    fn test_stdenv_path() {
+        let m =
+            build_cli().get_matches_from(vec!["nix-template", "stdenv", "-n", "-p", "mypackage", "pkgs/compilers/test/"]);
+        let expected = (
+            PathBuf::from("pkgs/compilers/test/mypackage/default.nix"),
+            PathBuf::from("../compilers/test/mypackage"),
+        );
+        assert_paths(m, expected);
+    }
+
 }
