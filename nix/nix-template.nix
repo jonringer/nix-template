b36be8b5dfc6fafc2a0355b9ad2680ac055e9f08
Jonathan Ringer <jonringer117@gmail.com>
2021-07-11 12:09:14 -0700

    Update nix expression

--- nix/nix-template.nix
+++ nix/nix-template.nix
@@ -1,4 +1,4 @@
-{ rustPlatform, nix-gitignore, clippy }:
+{ lib, rustPlatform, nix-gitignore, clippy, makeWrapper, nix, openssl, pkg-config }:
 
 rustPlatform.buildRustPackage rec {
    name = "nix-template";
@@ -6,7 +6,10 @@ rustPlatform.buildRustPackage rec {
    src = nix-gitignore.gitignoreSource [] ../.;
 
    # this will need to be updated anytime Cargo.lock gets changed
-   cargoSha256 = "sha256-/DT8avWBj1zx4SbsAOHSZlJlUql7VEeCLUlYvXon5Gg=";
+   cargoSha256 = "sha256-ooSZn57zebr+++/pf9z/ES0l+cTCsa5bnNAn67rG5cA=";
+
+   nativeBuildInputs = [ pkg-config makeWrapper ];
+   buildInputs = [ openssl ];
 
    doCheck = true;
    checkInputs = [ clippy ];
@@ -14,4 +17,9 @@ rustPlatform.buildRustPackage rec {
      cargo clippy
    '';
 
+   # needed for `nix-prefetch-url`
+   postInstall = ''
+     wrapProgram $out/bin/nix-template \
+       --prefix PATH : ${lib.makeBinPath [ nix ]}
+   '';
 }
